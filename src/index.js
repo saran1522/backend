6040fabffa18a1bb7c6c95786640a60a0949c82f
saran sinha <saransinha1522@gmail.com>
2024-04-19 17:57:12 +0530

    mongodb connection

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/dbconnect.js";
+
+dotenv.config({ path: "./env" });
+connectDB();
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error:", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// })();
