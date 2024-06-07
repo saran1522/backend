ce011643deda72852396a36c0bf20e8cc78de033
saran sinha <saransinha1522@gmail.com>
2024-06-07 17:16:20 +0530

    add user and video model with some jwt & bcrypt methods

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//we can use 'use' method with app to use middlewares
+//using different configurations below to communicate data in different formats
+//data may come in any formet, like json, text
+app.use(express.json({ limit: "20kb" }));
+//data url may be encoded, like name+age or name%20age
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+// data may be static files like images, pdf etc so we configure a folder for storing them
+app.use(express.static("public"));
+// we can also interact(CRUD) with cookies, so we use cookie-parser
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbconnect.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
 
 // import express from "express";
 // const app = express();
